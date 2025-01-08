86b14898a35b384734caac573e69156f40371332
Omer Faruk Ozer <ozeromerfaruk@gmail.com>
2025-01-08 15:03:56 +0300

    feat: Added new input system

--- Assets/_Scripts/Managers/PlayerInputManager.cs
+++ Assets/_Scripts/Managers/PlayerInputManager.cs
@@ -1,26 +1,57 @@
-using UnityEngine;
 using System;
+using UnityEngine;
 
-public class PlayerInputManager : MonoBehaviour
+namespace Managers
 {
-    public static event Action<Vector2> OnMouseClick;
-
-    private Camera m_MainCamera;
-
-
-    private void Awake()
+    public class PlayerInputManager : MonoBehaviour
     {
-        m_MainCamera = Camera.main;
-    }
+        public static event Action<Vector2> OnMouseClick;
 
-    private void Update()
-    {
-        HandleMouseInput();
-    }
+        private Camera m_MainCamera;
+        private PlayerInput m_PlayerInput;
 
-    private void HandleMouseInput()
-    {
-        if (Input.GetMouseButtonDown(0))
+
+        private void Awake()
+        {
+            m_MainCamera = Camera.main;
+            m_PlayerInput = new PlayerInput();
+        }
+
+        private void OnEnable()
+        {
+            m_PlayerInput.Enable();
+        }
+
+        private void Update()
+        {
+            HandleMouseInputNewInputSystem();       // Ney input system
+            // HandleMouseInputOldInputSystem();    // Old input system
+
+        }
+
+        private void OnDisable()
+        {
+            m_PlayerInput.Disable();
+        }
+
+
+        private void HandleMouseInputNewInputSystem()
+        {
+            if (m_PlayerInput.Board.SelectBlock.WasPressedThisFrame())
+            {
+                ProcessMouseClick();
+            }
+        }
+
+        private void HandleMouseInputOldInputSystem()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ProcessMouseClick();
+            }
+        }
+
+        private void ProcessMouseClick()
         {
             Vector2 mousePosition = m_MainCamera.ScreenToWorldPoint(Input.mousePosition);
             OnMouseClick?.Invoke(mousePosition);
